e81498f0c32ea307e98ef55f8f213d4c25401a4a
vberin <140951374+vberin@users.noreply.github.com>
2025-09-23 18:29:52 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+// Импортируем path для разрешения путей
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   site: "https://www.yourwebsite.com", // update me!
   integrations: [
@@ -12,4 +17,17 @@ export default defineConfig({
       priority: 0.7,
     }),
   ],
+  // --- ДОБАВЬТЕ ЭТОТ БЛОК ---
+  vite: {
+    resolve: {
+      alias: {
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@data": path.resolve(__dirname, "./src/data"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@js": path.resolve(__dirname, "./src/js"),
+      },
+    },
+  },
 });
